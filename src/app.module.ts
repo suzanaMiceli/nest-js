2da20e4d394062839de47719050d62da59b6ff79
Suzana <srn.suzana@gmail.com>
2023-04-12 12:00:24 -0300

    add: rate limit and secret to .env.example

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './user/user.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [forwardRef(() => UsersModule), forwardRef(() => AuthModule)],
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
+    forwardRef(() => UsersModule),
+    forwardRef(() => AuthModule),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
